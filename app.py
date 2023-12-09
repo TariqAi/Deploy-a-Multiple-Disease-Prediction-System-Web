9c935d8ca485d383a683726585d758212b14c1ee
Tariq Ibrahim <145915009+TariqAi@users.noreply.github.com>
2023-12-09 14:39:01 +0300

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ with st.sidebar:
     selected = option_menu('Multiple Disease Prediction System',
                           
                           ['Diabetes Disease',
-                           'Heart Disease ',
+                           'Heart Disease',
                            'Parkinsons Syndrome'],
                           icons=['activity','heart','person'],
                           default_index=0)
